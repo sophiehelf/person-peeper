8a32b43a9ad1d7c043ab8e38ff5018ed76bc16d8
sophiehelf <sophie.helf@gmail.com>
2017-10-18 22:31:13 -0400

    added some conversation

--- src/PersonList.js
+++ src/PersonList.js
@@ -9,14 +9,19 @@ class PersonList extends React.Component {
 		this.state = {
 			people: PersonData
 		}
+
+
 	}
 
 	render() {
 		console.log(this.state)
 		return(
+			<div id="container">
 			<ul>{this.state.people.map((person, key) => {
-				return <li key={key}>My name is {person.name.first} {person.name.last}. I am from {person.location.state} and currently reside in {person.location.city}.</li>
+				return <li key={key}>"My name is {person.name.first} {person.name.last}. I am from {person.location.state} and currently reside in {person.location.city}."</li>
 			})}</ul>
+			<p>{this.state.people[Math.floor(Math.random() * this.state.people.length)].name.first} steps forward and extends {this.state.people[Math.floor(Math.random() * this.state.people.length)].gender === "female" ? "her" : "his"} hand.</p>
+			</div>
 		)
 	}
 }
