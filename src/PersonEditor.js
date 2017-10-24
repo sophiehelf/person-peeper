0e552508192124dd755411daa9d0a7f051201e12
sophiehelf <sophie.helf@gmail.com>
2017-10-24 15:30:51 -0400

    some state changes working

--- src/PersonEditor.js
+++ src/PersonEditor.js
@@ -16,7 +16,7 @@ class PersonEditor extends React.Component {
 
 	}
 
-
+	
 
 
 	render() {
@@ -25,21 +25,20 @@ class PersonEditor extends React.Component {
 			<div>
 				<form name="person-form" onSubmit={this.props.handleInputChange}>
 					 <label>
-					 First Name: <input type="text" name="firstName" onSubmit={this.handleInputChange}/>
+					 First Name: <input type="text" name="firstName" value={this.props.firstName} onChange={this.props.handleFirstNameChange}/>
 					 </label>
 					 <br />
 					 <label> 
-					 Last Name: <input type="text" name="lastName" onSubmit={this.handleInputChange}/>
+					 Last Name: <input type="text" name="lastName" value={this.props.lastName} onChange={this.props.handleLastNameChange}/>
 					 </label>
 					 <br />
 					 <label>
-					 City: <input type="text" name="city" onSubmit={this.handleInputChange}/>
+					 City: <input type="text" name="city" value={this.props.city} onChange={this.props.handleCityChange}/>
 					 </label>
 					 <br />
 					 <label>
-					 State: <input type="text" name="theState" onSubmit={this.handleInputChange}/>
+					 State: <input type="text" name="theState" value={this.props.theState} onChange={this.props.handleTheStateChange}/>
 					 </label>
-					 <input type="submit" value="Click me"/> 
 				</form>
 			</div>
 		)
--- src/PersonShower.js
+++ src/PersonShower.js
@@ -14,22 +14,36 @@ class PersonShower extends React.Component {
 		}
 	}
 
-	handleInputChange(e) {
-		e.preventDefault();
-		debugger;
-		// console.log(e.target.value)
-		const target = e.target;
-		debugger;
-		console.log(target)
+
+	handleFirstNameChange = (e) => {
+		e.preventDefault()
+		this.setState({firstName: e.target.value})
+	}
+
+	handleLastNameChange = (e) => {
+		e.preventDefault()
+		this.setState({lastName: e.target.value})
+	}
+
+	handleCityChange = (e) => {
+		e.preventDefault()
+		this.setState({city: e.target.value})
+	}
+
+	handleTheStateChange = (e) => {
+		e.preventDefault()
+		this.setState({theState: e.target.value})
 	}
 
 
 	render() {
 
+		console.log(this.props)
+
 		return(
 			<div>
 		<div id="person-container"><img src={this.state.picture} alt="person-img"/><br /><br />My name is {this.state.firstName.charAt(0).toUpperCase() + this.state.firstName.slice(1)} {this.props.person.name.last.charAt(0).toUpperCase() + this.props.person.name.last.slice(1)} and I was born in {this.props.person.location.state.charAt(0).toUpperCase() + this.props.person.location.state.slice(1)}. I currently live in {this.props.person.location.city.charAt(0).toUpperCase() + this.props.person.location.city.slice(1)}.<br /><br /> <br />
-			<PersonEditor handleInputChange={this.handleInputChange.bind(this)}  name="person-form" person={this.props.person}/>
+			<PersonEditor name="person-form" handleFirstNameChange={this.handleFirstNameChange} handleLastNameChange={this.handleLastNameChange} handleCityChange={this.handleCityChange} handleTheStateChange={this.handleTheStateChange} person={this.props.person}/>
 		</div>
 	</div>
 		)
